5c9493172bb480c721b0beb2eb96c0cb6c5bfcb5
maniact <773926819@qq.com>
2022-04-08 09:11:44 +0800

    maniact

---
+++ hw03/Neighbor Digits.py
@@ -0,0 +1,30 @@
+def neighbor_digits(num, prev_digit=-1):
+    """
+    Returns the number of digits in num that have the same digit to its right
+    or left.
+    >>> neighbor_digits(111)
+    3
+    >>> neighbor_digits(123)
+    0
+    >>> neighbor_digits(112)
+    2
+    >>> neighbor_digits(1122)
+    4
+    """
+    "*** YOUR CODE HERE ***"
+    num =list(str(num))
+    count = 0
+    for x in num[1:]:
+        if (x == num[0]):
+            count = count + 1
+    for x in num[:len(num)-1:-1]:
+        if (x == num[-1]):
+            count = count + 1 
+    return count
+        
+
+
+
+if __name__=='__main__':
+    import doctest
+    doctest.testmod(verbose=True)
