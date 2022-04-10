998887b2edce53cc3abd9c24aab60a3993be352c
maniact <773926819@qq.com>
2022-04-10 12:30:29 +0800

    maniact

--- hw03/Neighbor Digits.py
+++ hw03/Neighbor Digits.py
@@ -12,19 +12,13 @@ def neighbor_digits(num, prev_digit=-1):
     4
     """
     "*** YOUR CODE HERE ***"
-    num =list(str(num))
-    count = 0
-    for x in num:
-        if (x == num[0]):
-            count = count + 1
-    for x in num[::-1]:
-        if (x == num[-1]):
-            count = count + 1 
-    return count
-        
+    if num < 10:
+        return num == prev_digit  # 如果只有一位数直接返回-1作为递归的出口
+    last = num % 10  # 获取num的最低位
+    rest = num // 10  # 获取num最高位
+    return int(prev_digit == last or rest % 10 == last) + neighbor_digits(num // 10, last)  # 返回相同数位和
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     import doctest
     doctest.testmod(verbose=True)
