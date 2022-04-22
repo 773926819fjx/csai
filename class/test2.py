59396b055df1256ebd1886e09290bb26cd525deb
maniact <773926819@qq.com>
2022-04-22 09:33:32 +0800

    maniact

--- class/test2.py
+++ class/test2.py
@@ -25,28 +25,29 @@
 #     def clean(self):
 #         self.is_clean = True
 
-class StudentGrade:
-    """
-    >>> grade1 = StudentGrade("Arfur Artery", 300)
-    >>> grade1.is_failing()
-    False
-    >>> grade2 = StudentGrade("MoMo OhNo", 158)
-    >>> grade2.is_failing()
-    True
-    >>> grade1.failing_grade
-    159
-    >>> grade2.failing_grade
-    159
-    >>> StudentGrade.failing_grade
-    159
-    >>>
-    """    
-    failing_grade = 159
-    def __init__(self, student_name, num_points):
-        self.student_name = student_name
-        self.num_points = num_points
-    def is_failing(self):
-        return self.num_points < StudentGrade.failing_grade
+# class StudentGrade:
+#     """
+#     >>> grade1 = StudentGrade("Arfur Artery", 300)
+#     >>> grade1.is_failing()
+#     False
+#     >>> grade2 = StudentGrade("MoMo OhNo", 158)
+#     >>> grade2.is_failing()
+#     True
+#     >>> grade1.failing_grade
+#     159
+#     >>> grade2.failing_grade
+#     159
+#     >>> StudentGrade.failing_grade
+#     159
+#     >>>
+#     """    
+#     failing_grade = 159
+#     def __init__(self, student_name, num_points):
+#         self.student_name = student_name
+#         self.num_points = num_points
+#     def is_failing(self):
+#         return self.num_points < StudentGrade.failing_grade
+
         
 if __name__=='__main__':
     import doctest
