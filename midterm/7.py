10ed45ccf18c2b062acd731b7c9fe019aa2d270b
AI一班2025030129樊嘉翔 <773926819@qq.com>
2022-05-20 01:27:01 +0000

    上传新文件

---
+++ midterm/7.py
@@ -0,0 +1,12 @@
+a = {'mayun':'13309283335',
+    'zhaolong':'18989227822',
+    'zhangmin':'13382398921',
+    'Gorge':'19833824743',
+    'Jordan':'18807317878',
+    'Curry':'15093488129',
+    'Wade':'19282937665'}
+b = input()
+if b in a:
+    print(a[b])
+else:
+    print('not found')
