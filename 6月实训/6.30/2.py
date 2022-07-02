b03717ca319f43dc2a641a5dff7a5a85b40a7739
maniact <773926819@qq.com>
2022-07-02 15:11:00 +0800

    maniact

--- 6月实训/6.30/2.py
+++ 6月实训/6.30/2.py
@@ -3,13 +3,13 @@ from pandas.io.excel import ExcelWriter
 import pandas as pd
 from pandas import DataFrame
 
-# wlist = [['月份', '营业额'], ['一月', 5555], ['二月', 6666], ['三月', 7777], ['四月', 4545],
-#          ['五月', 2565], ['六月', 5656], ['七月', 8321], ['八月', 4545], ['九月', 2165],
-#          ['十月', 3656], ['十一月', 7575], ['十二月', 1526]]
+wlist = [['月份', '营业额'], ['一月', 5555], ['二月', 6666], ['三月', 7777], ['四月', 4545],
+         ['五月', 2565], ['六月', 5656], ['七月', 8321], ['八月', 4545], ['九月', 2165],
+         ['十月', 3656], ['十一月', 7575], ['十二月', 1526]]
 
-# with open('营业额.csv', 'w', newline='', encoding='utf-8') as cfile:
-#     fwrite = csv.writer(cfile)
-#     fwrite.writerows(wlist)
+with open('营业额.csv', 'w', newline='', encoding='utf-8') as cfile:
+    fwrite = csv.writer(cfile)
+    fwrite.writerows(wlist)
 
 with ExcelWriter('营业额.xlsx') as file:
     # 将csv文件转换为excel文件
